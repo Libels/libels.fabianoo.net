455e66ed5c9db43062be4a8990d9d15f04090482
Fray117 <5698303-Fray117@users.noreply.gitlab.com>
2022-07-13 12:51:46 +0700

    styling control

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [],
+	theme: {
+		extend: {
+			colors: {
+				'warm-gray': colors.warmGray,
+				cyan: colors.cyan,
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+	],
 }
