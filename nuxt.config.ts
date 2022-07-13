a2488bcea5c65ce18055fb25914efc457f699941
Fray117 <5698303-Fray117@users.noreply.gitlab.com>
2022-07-13 14:21:42 +0700

    build config fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	target: 'static',
-	modules: ['@nuxtjs/tailwindcss']
+	modules: ['@nuxtjs/tailwindcss'],
+	build: {
+		transpile: [
+			'@headlessui/vue'
+		]
+	}
 })
