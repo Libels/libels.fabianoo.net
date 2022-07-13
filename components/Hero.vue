d48a000075841b355c8392a482a49d791f54512f
Fray117 <5698303-Fray117@users.noreply.gitlab.com>
2022-07-13 12:52:00 +0700

    hero section update

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,15 +1,23 @@
 <template>
-	<div class="grid grid-cols-7 mt-32">
-		<div class="col-span-3">
+	<div class="grid grid-cols-7 gap-8 mt-32">
+		<div class="col-span-7 sm:col-span-3">
 			<h1 class="text-4xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">Libels Computer Club</h1>
 			<p class="text-lg sm:text-xl mb-4 mt-8">Libels Computer Club merupakan salah satu Ekstrakulikuler yang
 				terdiri dari
 				manusia pegiat komputer di SMAN 15 Jakarta.</p>
-			<div class="flex text-white font-bold bg-purple-600 rounded-md justify-between w-36">
-				<div class="p-2">Yuk Gabung</div>
-				<div class="bg-fuchsia-400 p-2 rounded-md">&gt;</div>
+			<div class="flex text-white font-bold bg-purple-600 rounded-md justify-between">
+				<div class="px-8 py-3 md:py-4 md:text-lg md:px-10">Yuk Gabung</div>
+				<div class="bg-fuchsia-400 px-8 py-3 md:py-4 md:text-lg md:px-10 rounded-md">&gt;</div>
 			</div>
 		</div>
-		<div>LCC</div>
+		<img src="/artboard/hero.png" alt="LCC Logo" class="hidden sm:block w-full h-72 object-contain col-span-4">
 	</div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@900&display=swap');
+
+h1 {
+	font-family: 'Rubik', sans-serif;
+}
+</style> 
