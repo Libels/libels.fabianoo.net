3f9f4023e74940a089dfd1a5c2c787557965d082
Fray117 <5698303-Fray117@users.noreply.gitlab.com>
2022-07-13 14:06:16 +0700

    rounding more

--- components/Showcase.vue
+++ components/Showcase.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="bg-blue-50 mt-10 pt-6 pb-12 sm:rounded-md relative overflow-hidden">
+	<div class="bg-blue-50 mt-10 pt-20 pb-24 sm:rounded-lg relative overflow-hidden">
 		<svg class="absolute top-0 left-full transform -translate-x-1/2 -translate-y-3/4 lg:left-auto lg:right-full lg:translate-x-2/3 lg:translate-y-1/4"
 			width="404" height="784" fill="none" viewBox="0 0 404 784" aria-hidden="true">
 			<defs>
@@ -13,9 +13,9 @@
 		<h2 class="text-center text-4xl mb-4 font-bold">Kita bisa apa?</h2>
 		<div class="grid grid-cols-3 gap-4 sm:px-8">
 			<div v-for="item in items" class="col-span-3 lg:col-span-1 flex lg:block relative lg:shadow-md">
-				<img :src="item.thumbnails[0]" :alt="`${item.title} preview`" class="sm:rounded-md z-0">
+				<img :src="item.thumbnails[0]" :alt="`${item.title} preview`" class="sm:rounded-lg z-0">
 				<div
-					class="bg-purple-600 text-white rounded-l-lg sm:rounded-md px-4 py-2 absolute right-0 lg:bottom-0 h-full lg:h-auto w-1/3 lg:w-full z-10">
+					class="bg-purple-600 text-white rounded-l-lg sm:rounded-lg px-4 py-2 absolute right-0 lg:bottom-0 h-full lg:h-auto w-1/3 lg:w-full z-10">
 					<h3 class="font-medium text-lg leading-6 ">{{ item.title }}</h3>
 					<p class="text-base ">{{ item.description }}</p>
 				</div>
